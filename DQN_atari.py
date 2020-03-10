8ed06f310d7e86110fad3d9cf6927bfadae26a4b
Barkin Buyukcakir <barkinbuyukcakir@gmail.com>
2020-03-10 11:59:17 +0300

    Figured out how to train the CNN to provide Q values

--- DQN_atari.py
+++ DQN_atari.py
@@ -1,15 +1,31 @@
-import numpy as np
 import gym
 import torch
 import torch.nn.functional as F
-import matplotlib.pyplot as plt
-from torch.nn import Conv2d
+import collections
+import numpy as np
 
 torch.manual_seed(42)
 
 
+class ReplayMemory:
+
+    def __init__(self, max_size):
+        self.mem = collections.deque(maxlen=max_size)
+
+    def sample(self, size):
+        length = len(self.mem)
+        samp = np.random.choice(np.arange(size), size=size, replace=False)
+        print(samp)
+        return [self.mem[index] for index in samp]
+
+    def add(self, x):
+        self.mem.append(x)
+
+    def __setitem__(self, key, value):
+        self.mem[key] = value
+
+
 class Model(torch.nn.Module):
-    conv1: Conv2d
 
     def __init__(self):
         super(Model, self).__init__()
@@ -20,21 +36,70 @@ class Model(torch.nn.Module):
         self.fc2 = torch.nn.Linear(64, 4)
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        y = self.pool(F.relu(self.conv2(x)))
-        z = (self.fc1(y.view(-1, 30 * 10 * 7)))
+        self.convout1 = self.pool(F.relu(self.conv1(x)))
+        self.convout2 = self.pool(F.relu(self.conv2(self.convout1)))
+        z = (self.fc1(self.convout2.view(-1, 30 * 10 * 7)))
         return torch.sigmoid(self.fc2(z))
 
-    def loss(self, y, y_pred):
-        F.mse_loss()
+    # def loss(self, y, y_pred):
+    #     F.mse_loss()
 
 
-fig, axs = plt.subplots(nrows=1, ncols=3)
-m = Model().float()
+# initialize function approximator and target models
+function_m = Model()
+target_m = Model()
+# initialize replay memory with capacity 1000
+memory_size = 1000
+replay_memory = ReplayMemory(memory_size)
+
+episodes = 3000
+batch_size = 100
+discount = 0.1
+
 env = gym.make('Breakout-v0')
 env.reset()
-prev_state = env.env.ale.getScreenRGB()
-out = m.forward(torch.tensor(prev_state).view(1, 3, 210, 160).float())
+# initialize Replay Memory and populate it
+for i in range(memory_size):
+    prev_state = env.env.ale.getScreenRGB()
+    action = env.action_space.sample()
+    next_state, reward, done, _ = env.step(action)
+    replay_memory.add((prev_state, action, reward, done, next_state))
+done = False
+i = 99
+j = 0
+for ep in range(episodes):
+    while not done:
+        prev_state = env.env.ale.getScreenRGB()
+        action = env.action_space.sample()
+        next_state, reward, done, _ = env.step(action)
+        replay_memory[i % 1000] = (prev_state, action, reward, done, next_state)
+        i += 1
 
-J = m.loss()
+        # every 100 steps, take a gradient descent step
+        if i % 100 == 0:
+            batch = replay_memory.sample(batch_size)
+            for sample in batch:
+                input_, action_, reward_, done_, next_state_ = sample
+                preds = function_m.forward(torch.tensor(input_).view(1, 3, 210, 160).float())
+                next_qs = target_m.forward(torch.tensor(next_state_).view(1, 3, 210, 160).float())
+                if done_:
+                    lab = reward_
+                else:
+                    maxval, argmax = torch.max(next_qs, -1)
+                    lab = reward_ + discount * argmax
+                    labels=[]
+                    # TODO: set all labels same as pred except for argmax, where it is set to lab
+                loss = F.mse_loss(preds, labels)
+                loss.backward()
+            with torch.no_grad():
+                pass
+        # every thousand steps, update the target network weights
+        if i % 1000 == 0:
+            with torch.no_grad():
+                target_m[0].conv1.weight = function_m[0].conv1.weight
+                target_m[0].conv2.weight = function_m[0].conv2.weight
+                target_m[0].fc1.weight = function_m[0].fc1.weight
+                target_m[0].fc2.weight = function_m[0].fc2.weight
 
+    env.reset()
+    # Perform a single step of gradient descent
